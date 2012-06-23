414d0f47e575919db0943fcd69f6a256e1b78926
Kornelije Sajler <xajler@gmail.com>
2012-06-23 03:00:36 +0200

    Removed way of adding new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_url, notice: "Thank you for signing up!"
-    else
-      render "new"
-    end
+    render "new"
+    # @user = User.new(params[:user])
+    # @user = nil
+    # if @user.save
+    #   session[:user_id] = @user.id
+    #   redirect_to root_url, notice: "Thank you for signing up!"
+    # else
+    #   render "new"
+    # end
   end
 end
