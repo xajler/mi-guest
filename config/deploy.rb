be823cca38f89e04317aa57ea1aa9e2bb9f065a6
Kornelije Sajler <xajler@gmail.com>
2013-05-04 08:43:24 +0200

    Changed github repo to be always mi-guest no matter what deployment it is.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:xajler/#{application}.git"
+# set :repository, "git@github.com:xajler/#{application}.git"
+set :repository, "git@github.com:xajler/mi-guest.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
